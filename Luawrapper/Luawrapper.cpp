0f55ca26bb77003a5117dbb3d96b7bac69b3a6c0
Vincenzo Cappello <vincenzo.cappello@gmail.com>
2020-04-09 18:10:04 +0200

    initial

---
+++ Luawrapper/Luawrapper.cpp
@@ -0,0 +1,9 @@
+// Luawrapper.cpp : Questo file contiene la funzione 'main', in cui inizia e termina l'esecuzione del programma.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
