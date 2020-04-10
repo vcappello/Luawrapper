8312b4eacc220716f132ccfa384c54b1fb4bff0d
Vincenzo Cappello <vincenzo.cappello@gmail.com>
2020-04-10 12:39:09 +0200

    c++ class binding

--- Luawrapper/Luawrapper.cpp
+++ Luawrapper/Luawrapper.cpp
@@ -7,7 +7,6 @@
 
 int CSum(int n1, int n2)
 {
-	std::cout << "[CSum (" << n1 << ", " << n2 << ")]" << std::endl;
 	return n1 + n2;
 }
 
@@ -16,8 +15,10 @@ std::string CConcat(std::string s1, std::string s2)
 	return s1 + s2;
 }
 
-void test_reg_fun()
+void test_function()
 {
+	std::cout << "* Test Function" << std::endl;
+
 	lua_State* L = luaL_newstate();
 	luaL_openlibs(L);
 
@@ -39,7 +40,61 @@ void test_reg_fun()
 	lua_close(L);
 }
 
+class CClass
+{
+public:
+	int sum(int n1, int n2)
+	{
+		return n1 + n2;
+	}
+
+	std::string concat(std::string s1, std::string s2)
+	{
+		return s1 + s2;
+	}
+};
+
+namespace lua
+{
+	template<>
+	struct ClassParam<CClass>
+	{
+		static constexpr char lua_name[] = "CClass";
+
+		inline static const std::vector<luaL_Reg> mem_funs = { 
+			{ "sum", [](lua_State* L)->int { return lua::reg_mem_fun(L, &CClass::sum); } },
+			{ "concat", [](lua_State* L)->int { return lua::reg_mem_fun(L, &CClass::concat); } }
+		};
+	};
+}
+
+void test_class()
+{
+	std::cout << "* Test Class" << std::endl;
+
+	lua_State* L = luaL_newstate();
+	luaL_openlibs(L);
+
+	CClass* obj = new CClass();
+	lua::Type<CClass>::push(L, obj);
+	lua_setglobal(L, "CObj");
+
+	luaL_loadstring(L, R"***(
+		local result = CObj:sum(1, 2)
+		print (result)
+
+		local str = CObj:concat('Hello', 'World')
+		print (str)
+	)***");
+
+	lua_pcall(L, 0, 0, 0);
+
+	lua_close(L);
+}
+
 int main()
 {
-	test_reg_fun();
+	test_function();
+
+	test_class();
 }
--- Luawrapper/Luawrapper.h
+++ Luawrapper/Luawrapper.h
@@ -10,7 +10,7 @@ namespace lua
 	  LUA types
 	====================================================*/
 
-	template<typename T>
+	template<typename T, typename Enable = void>
 	struct Type
 	{
 		static constexpr bool is_lua_type = false;
@@ -69,28 +69,52 @@ namespace lua
 	  User defined class types
 	====================================================*/
 
-	//template<typename TClass>
-	//struct ClassParam
-	//{
-	//	static constexpr char lua_name[] = nullptr;
-	//};
+	template<typename TClass>
+	struct ClassParam
+	{
+		static constexpr char lua_name[] = nullptr;
 
-	//template<typename T>
-	//struct Type<T, std::enable_if_t<std::is_class<T>::value>>
-	//{
-	//	static constexpr bool is_lua_type = true;
+		static std::vector<luaL_Reg> mem_funs;
+	};
 
-	//	//static void push(lua_State* lua, T* value) { ... }
-	//	//static T* get(lua_State* lua, int index) { return ...; }
-	//	static T* check(lua_State* lua, int arg) { return (*reinterpret_cast<T**>(luaL_checkudata(lua, arg, ClassParam<T>::lua_name))); }
-	//	static T* type_check(lua_State* lua, int arg) { return (*reinterpret_cast<T**>(luaL_testudata(lua, arg, ClassParam<T>::lua_name))); }
-	//};
+	template<typename T>
+	struct Type<T, std::enable_if_t<std::is_class<T>::value>>
+	{
+		static constexpr bool is_lua_type = true;
+
+		static void push(lua_State* lua, T* value) 
+		{  
+			// Create a new metatable
+			luaL_newmetatable(lua, ClassParam<T>::lua_name);
+			// Metatable index for objects
+			lua_pushvalue(lua, -1);
+			lua_setfield(lua, -2, "__index");
+			// Set all methods
+			for (auto r : ClassParam<T>::mem_funs)
+			{
+				lua_pushcfunction(lua, r.func);
+				lua_setfield(lua, -2, r.name);
+			}
+			// Pop the metatable from the stack
+			lua_pop(lua, 1);
+			// Put the object in the stack
+			*reinterpret_cast<T**>(lua_newuserdata(lua, sizeof(T*))) = value;
+			luaL_setmetatable(lua, ClassParam<T>::lua_name);
+		}
+
+		//static T* get(lua_State* lua, int index) { return ...; }
+		static T* check(lua_State* lua, int arg) { return (*reinterpret_cast<T**>(luaL_checkudata(lua, arg, ClassParam<T>::lua_name))); }
+		static T* type_check(lua_State* lua, int arg) { return (*reinterpret_cast<T**>(luaL_testudata(lua, arg, ClassParam<T>::lua_name))); }
+	};
 
 	/*==================================================
 	  Register C/C++ function
 
 	  Usage:
-		reg_fun(L, my_func);
+		lua::set_global_fun(L, {
+			{ "CSum", [](lua_State* L)->int { return lua::reg_fun(L, CSum); } },
+			{ "CConcat", [](lua_State* L)->int { return lua::reg_fun(L, CConcat); } }
+		});
 	====================================================*/
 
 	// At the end call the C function
@@ -129,4 +153,37 @@ namespace lua
 			lua_setglobal(L, r.name);
 		}
 	}
+
+	/*==================================================
+	  Register C/C++ member function
+	  Usage:
+		reg_mem_fun(L, &my_class::my_method);
+	====================================================*/
+
+	// At the end call the C function
+	template <typename TObject, typename TRetVal, typename... TFunArgs, typename... TEvaluatedArgs>
+	int reg_mem_fun(lua_State* L, int, TObject* obj_ptr, TRetVal(TObject::* fn)(TFunArgs... args), TRetVal(TObject::*)(), TEvaluatedArgs... evaluated_args) {
+		auto ret = (obj_ptr->*fn)(std::forward<TEvaluatedArgs>(evaluated_args)...);
+		Type<TRetVal>::push(L, ret);
+		return 1;
+	}
+
+	// Extract the nth argument and, if there are more arguments call itself, if this is the last argument go to
+	// call the c function
+	template <typename TObject, typename TRetVal, typename... TFunArgs, typename TArg0, typename... TOtherArgs, typename... TEvaluatedArgs>
+	typename std::enable_if<Type<TArg0>::is_lua_type, int>::type
+		reg_mem_fun(lua_State* L, int i, TObject* obj_ptr, TRetVal(TObject::* fn)(TFunArgs... args), TRetVal(TObject::*)(TArg0, TOtherArgs... other_values), TEvaluatedArgs... evaluated_args) {
+		TRetVal(TObject:: * other_fn)(TOtherArgs... other_values) = nullptr;
+		auto val = Type<TArg0>::get(L, i);
+		return reg_mem_fun(L, i + 1, obj_ptr, fn, other_fn, std::forward<TEvaluatedArgs>(evaluated_args)..., val);
+	}
+
+	// Helper function, entry point
+	// Call like: reg_fun(L, global_func);
+	template <typename TObject, typename TRetVal, typename... TFunArgs>
+	int reg_mem_fun(lua_State* L, TRetVal(TObject::* fn)(TFunArgs... args)) {
+		auto obj_ptr = Type<TObject>::check(L, 1);
+		return reg_mem_fun(L, 2, obj_ptr, fn, fn);
+	}
+
 }
